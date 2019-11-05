014c3cac55605d1588b565273fcecac55e781bbe
danoy7 <57357786+danoy7@users.noreply.github.com>
2019-11-05 20:05:05 +0700

    Update Gadai.java
    Minor update

--- SIMPENAN_Gadai/src/com/pegadaian/Gadai.java
+++ SIMPENAN_Gadai/src/com/pegadaian/Gadai.java
@@ -1,23 +1,25 @@
+package com.pegadaian.git;
+
 import java.util.Scanner;
 
 public class Gadai {
-	public boolean filterGadai (String name,String katagori, String des, int price) {
+	public boolean filterGadai (String name,String category, String description, int price) {
 		boolean status =false;
 		boolean stnama = false;
 		boolean stcategory = false;
 		boolean stprice = false;
-		boolean stdes = false;
+		boolean stdescription = false;
 			try {
 				Scanner input = new Scanner (System.in);
 				
-				System.out.println("-------- Masukan Data yang di perlukan-------");
+				System.out.println("-------- Masukan Data yang Diperlukan-------");
 				System.out.print("Nama : ");
 				//String name = input.next();
-				System.out.print("Produk Category :");
-				//String katagori = input.next();
+				System.out.print("Product Category :");
+				//String category = input.next();
 				//input.nextLine();
-				//System.out.print("Deskription :");
-				//String des = input.nextLine();
+				//System.out.print("Description :");
+				//String description = input.nextLine();
 				//System.out.println("Price :");
 				//int price = input.nextInt();
 				if ((name.length()>3 )&&(name.length()<15) ) {
@@ -26,14 +28,14 @@ public class Gadai {
 					status = false;
 					//System.out.println("Nama Harus lebih dari 3 dan kurang dari 15 karakter");
 				}
-				if ((katagori.equalsIgnoreCase("laptop"))||(katagori.equalsIgnoreCase("motor"))||(katagori.equalsIgnoreCase("emas"))) {
+				if ((category.equalsIgnoreCase("laptop"))||(category.equalsIgnoreCase("motor"))||(katagori.equalsIgnoreCase("emas"))) {
 					stcategory=true;
 				}else {
 					//System.out.println("Barang gadai harus diantara barang berikut : Laptop, Motor, Emas");
 					status = false;
 				}
-				if(des.contains(" ")==true){
-					stdes=true;
+				if(description.contains(" ")==true){
+					stdescription=true;
 				}else {
 					//System.out.println("deskripsi harus lebih dari dua kata");
 					status = false;
@@ -44,7 +46,7 @@ public class Gadai {
 					//System.out.println("harus kelipatan 10000");
 					status = false;
 				}
-				if((stnama)&&(stcategory)&&(stdes)&&(stprice)) {
+				if((stnama)&&(stcategory)&&(stdescription)&&(stprice)) {
 					status=true;
 				}else {
 					status = false;
