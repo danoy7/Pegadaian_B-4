c13b089a322301d77af079994cb40dfff29eb810
Anorever <ameliasinta052@gmail.com>
2019-11-06 00:36:16 +0700

    Update main

--- SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
+++ SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
@@ -1,40 +1,28 @@
 package com.pegadaian.git;
-import com.pegadaian.git.Gadai;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MainGIT {
-	public GadaiMenu gadaimenu;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int Menus;
-		boolean stop = false;
-		Scanner sc = new Scanner(System.in);
+		System.out.println("Pegadaian System");
+		System.out.println("==========================");
 		String formatter = ("|%1$-25s|");
-		String specialFormatter = "|%1$-5s|%2$-15s|%3$-16s|%4$-20s|%5$-16s|";
+		System.out.println(String.format(formatter,"1. Gadai"));
+		System.out.println(String.format(formatter,"2. Tebus"));
+		System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
+		System.out.println(String.format(formatter,"4. Exit"));
+		System.out.println("Opsi : ");
+		Scanner sc = new Scanner(System.in);
+		Menus = sc.nextInt();
 		ArrayList inputgadai = new ArrayList();
-		while(stop==false) {
-			try {
-				System.out.println("Pegadaian System");
-				System.out.println("==========================");
-				System.out.println(String.format(formatter,"1. Gadai"));
-				System.out.println(String.format(formatter,"2. Tebus"));
-				System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
-				System.out.println(String.format(formatter,"4. Exit"));
-				System.out.println("Opsi : ");
-				Menus = sc.nextInt();
-				switch(Menus) {
-				case 1 :
-					inputgadai.add(Gadai.gadai());
-					
-					break;
-				case 2 :
-					System.out.println("-----------------------------------------------------------------------------");
-					System.out.println(String.format(specialFormatter, "ID","Product","Price", "Status","Utang"));
-					System.out.println("-----------------------------------------------------------------------------");
-					KelasTebus tebus = new KelasTebus();
+		switch(Menus) {
+			case 1 :
+				inputgadai.add(Gadai.gadai());
+				break;
+			case 2 :
+				KelasTebus tebus = new KelasTebus();
 					ArrayList listInput = tebus.OrderID(inputgadai);
 					tebus.ViewTebus(listInput);
 					boolean stopcase2 = false;
@@ -53,26 +41,17 @@ public class MainGIT {
 						sc.next();
 						continue;
 					}
-					};
-					break;
-				case 3 :
-					System.out.println("-----------------------------------------------------------------------------");
-					System.out.println(String.format(specialFormatter, "ID","Nama","Product", "Description","Price"));
-					System.out.println("-----------------------------------------------------------------------------");
-					InventoryBarang InventBarang = new InventoryBarang();
-					InventBarang.Inventory(inputgadai);					
-					break;
-				case 4 :
-					stop = true;
-					break;
-				default:
-				}
-			}catch(Exception e) {
-				System.out.println("menu yang anda pilih tidak terdapat di sistem");
-				sc.next();
-				continue;
-			}
-		};
+					};		
+				break;
+			case 3 :
+				InventoryBarang InventBarang = new InventoryBarang();
+				InventBarang.Inventory(inputgadai);					
+				break;
+			case 4 :
+				stop = true;
+				break;
+		default:
+		}
 	}
 
 }
