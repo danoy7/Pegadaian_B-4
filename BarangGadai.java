3d17e57a336c8f9e9a9dcac65c0995b42f493aef
Erin Dwi Fentika <erinfentika@gmail.com>
2019-11-05 14:43:24 +0700

    Create BarangGadai.java

---
+++ BarangGadai.java
@@ -0,0 +1,41 @@
+package com.pegadaian.git;
+
+public class BarangGadai {
+	String name;
+	String product_category;
+	String description;
+	int price;
+	public BarangGadai(String name, String product_category, String description, int price) {
+		super();
+		this.name = name;
+		this.product_category = product_category;
+		this.description = description;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getProduct_category() {
+		return product_category;
+	}
+	public void setProduct_category(String product_category) {
+		this.product_category = product_category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+
+}
