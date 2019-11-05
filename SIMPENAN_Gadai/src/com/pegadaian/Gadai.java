f4629c8eb174223ea6eb558eb24fe84d6f49f43e
Ananda2255 <57357657+Ananda2255@users.noreply.github.com>
2019-11-05 17:50:39 +0700

    Update Verifikasi

--- SIMPENAN_Gadai/src/com/pegadaian/Gadai.java
+++ SIMPENAN_Gadai/src/com/pegadaian/Gadai.java
@@ -1,58 +1,58 @@
 import java.util.Scanner;
 
 public class Gadai {
-	public static void main(String[] args) {
-		boolean status =true;
+	public boolean filterGadai (String name,String katagori, String des, int price) {
+		boolean status =false;
 		boolean stnama = false;
 		boolean stcategory = false;
 		boolean stprice = false;
 		boolean stdes = false;
-		while (status) {
 			try {
 				Scanner input = new Scanner (System.in);
 				
 				System.out.println("-------- Masukan Data yang di perlukan-------");
 				System.out.print("Nama : ");
-				String name = input.next();
+				//String name = input.next();
 				System.out.print("Produk Category :");
-				String katagori = input.next();
-				input.nextLine();
-				System.out.print("Deskription :");
-				String des = input.nextLine();
-				System.out.println("Price :");
-				int price = input.nextInt();
+				//String katagori = input.next();
+				//input.nextLine();
+				//System.out.print("Deskription :");
+				//String des = input.nextLine();
+				//System.out.println("Price :");
+				//int price = input.nextInt();
 				if ((name.length()>3 )&&(name.length()<15) ) {
 					stnama=true;
 				}else {
-					System.out.println("Nama Harus lebih dari 3 dan kurang dari 15 karakter");
+					status = false;
+					//System.out.println("Nama Harus lebih dari 3 dan kurang dari 15 karakter");
 				}
 				if ((katagori.equalsIgnoreCase("laptop"))||(katagori.equalsIgnoreCase("motor"))||(katagori.equalsIgnoreCase("emas"))) {
 					stcategory=true;
 				}else {
-					System.out.println("Barang gadai harus diantara barang berikut : Laptop, Motor, Emas");
+					//System.out.println("Barang gadai harus diantara barang berikut : Laptop, Motor, Emas");
+					status = false;
 				}
 				if(des.contains(" ")==true){
 					stdes=true;
 				}else {
-					System.out.println("deskripsi harus lebih dari dua kata");
+					//System.out.println("deskripsi harus lebih dari dua kata");
+					status = false;
 				}
 				if (price %10000 == 0) {
 					stprice=true;
 				}else {
-					System.out.println("harus kelipatan 10000");
+					//System.out.println("harus kelipatan 10000");
+					status = false;
 				}
 				if((stnama)&&(stcategory)&&(stdes)&&(stprice)) {
-					status=false;
+					status=true;
 				}else {
-					status = true;
+					status = false;
 				}
 			}catch(Exception e) {
-				System.out.println("Tolong Isi price Dengan Integer");
+				status = false;
 			}
-			
-		}
-		
-		
+			 return status;
 		
 	}
 }
