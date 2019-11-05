2c784bbd9736799b3f73bf24ce1691607eff6414
febriany syafitri <52740052+FebrianysAzwar@users.noreply.github.com>
2019-11-05 15:10:35 +0700

    try catch
    jika masukan nya tidak terdapat di menu maka kesalahan ditangkap menggunakan try and catch kemudian menu akan ditampilkan lagi menggunakan fungsi perulangan while.

--- SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
+++ SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
@@ -6,25 +6,35 @@ public class MainGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int Menus;
-		System.out.println("Pegadaian System");
-		System.out.println("==========================");
-		String formatter = ("|%1$-25s|");
-		System.out.println(String.format(formatter,"1. Gadai"));
-		System.out.println(String.format(formatter,"2. Tebus"));
-		System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
-		System.out.println(String.format(formatter,"4. Exit"));
-		System.out.println("Opsi : ");
+		boolean stop = false;
 		Scanner sc = new Scanner(System.in);
-		Menus = sc.nextInt();
-		switch(Menus) {
-		case 1 :
-			break;
-		case 2 :
-			break;
-		case 3 :
-			break;
-		default:
-		}
+		while(stop==false) {
+			try {
+				System.out.println("Pegadaian System");
+				System.out.println("==========================");
+				String formatter = ("|%1$-25s|");
+				System.out.println(String.format(formatter,"1. Gadai"));
+				System.out.println(String.format(formatter,"2. Tebus"));
+				System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
+				System.out.println(String.format(formatter,"4. Exit"));
+				System.out.println("Opsi : ");
+				Menus = sc.nextInt();
+				switch(Menus) {
+				case 1 :
+					System.out.println();
+					break;
+				case 2 :
+					break;
+				case 3 :
+					break;
+				default:
+				}
+			}catch(Exception e) {
+				System.out.println("menu yang anda pilih tidak terdapat di sistem");
+				sc.next();
+				continue;
+			}
+		};
 	}
 
 }
