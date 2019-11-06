c7f17d6afc2572bed98e5b59fe3bb016b31424de
Anorever <ameliasinta052@gmail.com>
2019-11-06 07:54:35 +0700

    Fixing Kelas Main / MainMenu
    Merapikan Tampilan, Memperbaiki syntax dan Menambahkan perulangan while untuk menu setiap input 1,2,3 kemudian boolean menyatakan berhenti (stop=true) saat user memilih Menu 4 .

--- SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
+++ SIMPENAN_Gadai/src/com/pegadaian/git/MainGIT.java
@@ -1,28 +1,34 @@
 package com.pegadaian.git;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.*;
 
 public class MainGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int Menus;
-		System.out.println("Pegadaian System");
-		System.out.println("==========================");
-		String formatter = ("|%1$-25s|");
-		System.out.println(String.format(formatter,"1. Gadai"));
-		System.out.println(String.format(formatter,"2. Tebus"));
-		System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
-		System.out.println(String.format(formatter,"4. Exit"));
-		System.out.println("Opsi : ");
+		boolean stop = false;
 		Scanner sc = new Scanner(System.in);
-		Menus = sc.nextInt();
 		ArrayList inputgadai = new ArrayList();
-		switch(Menus) {
-			case 1 :
-				inputgadai.add(Gadai.gadai());
-				break;
-			case 2 :
-				KelasTebus tebus = new KelasTebus();
+		while(stop==false) {
+			try {
+				System.out.println("Pegadaian System");
+				System.out.println("==========================");
+				String formatter = ("|%1$-25s|");
+				System.out.println(String.format(formatter,"1. Gadai"));
+				System.out.println(String.format(formatter,"2. Tebus"));
+				System.out.println(String.format(formatter,"3. Tampilkan Inventory"));
+				System.out.println(String.format(formatter,"4. Exit"));
+				System.out.print("Opsi : ");
+				Menus = sc.nextInt();
+				switch(Menus) {
+				case 1 :
+					inputgadai.add(Gadai.gadai());
+					
+					break;
+				case 2 :
+					KelasTebus tebus = new KelasTebus();
 					ArrayList listInput = tebus.OrderID(inputgadai);
 					tebus.ViewTebus(listInput);
 					boolean stopcase2 = false;
@@ -41,17 +47,21 @@ public class MainGIT {
 						sc.next();
 						continue;
 					}
-					};		
-				break;
-			case 3 :
-				InventoryBarang InventBarang = new InventoryBarang();
-				InventBarang.Inventory(inputgadai);					
-				break;
-			case 4 :
-				stop = true;
-				break;
-		default:
-		}
-	}
-
-}
+					};
+					break;
+				case 3 :
+					InventoryBarang InventBarang = new InventoryBarang();
+					InventBarang.Inventory(inputgadai);					
+					break;
+				case 4 :
+					stop = true;
+					break;
+				default:
+				}
+			}catch(Exception e) {
+				System.out.println("menu yang anda pilih tidak terdapat di sistem");
+				sc.next();
+				continue;
+			}
+		};
+	}}
